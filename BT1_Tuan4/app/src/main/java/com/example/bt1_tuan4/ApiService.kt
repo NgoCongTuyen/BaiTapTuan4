edfcb4d2b76cb2f24defeb6325d0fbec1535159d
NgoCongTuyen <2251120260@ut.edu.vn>
2025-03-25 15:01:39 +0700

    up

---
+++ BT1_Tuan4/app/src/main/java/com/example/bt1_tuan4/ApiService.kt
@@ -0,0 +1,24 @@
+import com.example.bt1_tuan4.Task
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface ApiService {
+    @GET("tasks") // Đổi từ "task/1" thành "tasks" để lấy danh sách
+    suspend fun getTasks(): ApiResponseList
+    @GET("tasks/{id}") // Dùng id thay vì title
+    suspend fun getTaskDetail(@Path("id") id: String): ApiResponse<Task>
+}
+
+data class ApiResponse<T>(
+    val isSuccess: Boolean,
+    val message: String,
+    val data: T
+)
+
+
+
+data class ApiResponseList(
+    val isSuccess: Boolean,
+    val message: String,
+    val data: List<Task>
+)
